e5c16693a6af51bc89cfee248a1f9d6addcd464d
Simon <carbontwelve@users.noreply.github.com>
2025-11-02 23:57:11 +0000

    bugfix: set teamdesc from $_POST

--- teams.php
+++ teams.php
@@ -67,7 +67,7 @@ if (array_key_exists('teamname', $_POST) == true)
 $teamdesc = null;
 if (array_key_exists('teamdesc', $_POST) == true)
 {
-    $teamname = $_POST['teamdesc'];
+    $teamdesc = $_POST['teamdesc'];
 }
 
 $confirmed = null;
