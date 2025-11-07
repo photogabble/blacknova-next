88eae9334dcc67518db360459e84cd6f8e201556
Simon <carbontwelve@users.noreply.github.com>
2025-11-07 23:01:02 +0000

    refactor: this class felt like an enum

--- classes/Ports.php
+++ classes/Ports.php
@@ -1,6 +1,7 @@
 <?php
 // Blacknova Traders - A web-based massively multiplayer space combat and trading game
 // Copyright (C) 2001-2014 Ron Harwood and the BNT development team
+// Copyright (C) 2025 Simon Dann
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as
@@ -19,32 +20,24 @@
 
 namespace Bnt;
 
-class Ports
+enum Ports: string
 {
-    public static function getType($ptype, $langvars)
+    case ORE = 'ore';
+    case NONE = 'none';
+    case ENERGY = 'energy';
+    case ORGANICS = 'organics';
+    case GOODS = 'goods';
+    case SPECIAL = 'special';
+
+    public function getLocalizedName(array $langvars = []): string
     {
-        switch ($ptype)
-        {
-            case 'ore':
-                $ret = $langvars['l_ore'];
-                break;
-            case 'none':
-                $ret = $langvars['l_none'];
-                break;
-            case 'energy':
-                $ret = $langvars['l_energy'];
-                break;
-            case 'organics':
-                $ret = $langvars['l_organics'];
-                break;
-            case 'goods':
-                $ret = $langvars['l_goods'];
-                break;
-            case 'special':
-                $ret = $langvars['l_special'];
-                break;
-        }
-        return $ret;
+        return match ($this) {
+            self::ORE => $langvars['l_ore'] ?? Translate::get('common.l_ore'),
+            self::NONE => $langvars['l_none'] ?? Translate::get('common.l_none'),
+            self::ENERGY => $langvars['l_energy'] ?? Translate::get('common.l_energy'),
+            self::ORGANICS => $langvars['l_organics'] ?? Translate::get('common.l_organics'),
+            self::GOODS => $langvars['l_goods'] ?? Translate::get('common.l_goods'),
+            self::SPECIAL => $langvars['l_special'] ?? Translate::get('common.l_special'),
+        };
     }
 }
-?>
